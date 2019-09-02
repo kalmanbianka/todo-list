d3919d6a6349a65702ae5c58ba25107e0b3efade
Kálmán Bianka <kalman.bianka97@gmail.com>
2019-09-02 19:16:30 +0200

    added style to todo-item

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { TodoComponent } from './components/todo/todo.component';
 import { InputComponent } from './components/todo/input/input.component';
+import { TodoItemComponent } from './components/todo/todo-item/todo-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     TodoComponent,
-    InputComponent
+    InputComponent,
+    TodoItemComponent
   ],
   imports: [
     BrowserModule
